81935d74590c25aa1c95d0360948b00b27cbe853
Im-Dilasna-real <112766883+Im-Dilasna-real@users.noreply.github.com>
2023-10-06 19:08:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "2138073729:AAGGaGUCjYL--XuAasC6hQQNQ1e-6ABm-0M"
 
 # Project Name of your Project
 PROJECT_NAME = "GitGram"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/Im-Dilasna-real/GitGram"
